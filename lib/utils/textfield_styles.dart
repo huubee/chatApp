5e4ec52dc0b0d1789612e7fffe9a2cced4eb88f7
Hubert Huijbregts <info@huijbregts-bouw.nl>
2022-08-16 20:16:05 +0200

    Merge remote-tracking branch 'origin/5-03' into 5-04

--- lib/utils/textfield_styles.dart
+++ lib/utils/textfield_styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ThemeTextStyle {
-  static TextStyle loginTextFieldStyle = GoogleFonts.lato(
+  static TextStyle loginTextFieldStyle = GoogleFonts.alike(
     textStyle: TextStyle(
       fontSize: 18.0,
       color: Colors.grey,
