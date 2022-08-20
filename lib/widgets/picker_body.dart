3bf8ccb0b3a7f13db5164dbc9d8ded07cd7256b1
Hubert Huijbregts <info@huijbregts-bouw.nl>
2022-08-21 01:22:04 +0200

    Merge remote-tracking branch 'origin/6-08' into 6-09

--- lib/widgets/picker_body.dart
+++ lib/widgets/picker_body.dart
@@ -35,6 +35,11 @@ class NetworkImagePickerBody extends StatelessWidget {
                     },
                     child: Image.network(snapshot.data![index].urlSmallSize));
               });
+        } else if (snapshot.hasError) {
+          return Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text('This is the error: ${snapshot.error}'),
+          );
         }
 
         return const Padding(
